7b65642ced6eb49c312f4afd98c067aefa775d34
Gabriel <gabmineiro123@hotmail.com>
2026-09-02 17:46:47 -0400

    models de item

--- core/settings.py
+++ core/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Boa_Vista'
 
 USE_I18N = True
 
--- items/models.py
+++ items/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    register_date = models.DateTimeField("Item Register Date")
+    was_it_removed = models.BooleanField("Item Removed")
