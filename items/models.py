8cb0518393113db4a4fc207ad696b38b117de54c
gabmineiro123@hotmail.com <gabmineiro123@hotmail.com>
2026-09-02 20:11:12 -0400

    models criadas para itens e pessoas

--- items/models.py
+++ items/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    number_cellphone = models.CharField(max_length=20)
+    relationship = models.CharField(max_length=30)
+    cpf_person = models.CharField(max_length=30)
 class Item(models.Model):
-    item_name = models.CharField(max_length=200)
+    item_name = models.CharField(max_length=100)
     register_date = models.DateTimeField("Item Register Date")
+    withdraw_date = models.DateTimeField("Item Withdraw Date", null=True, blank=True)
     was_it_removed = models.BooleanField("Item Removed")
+    found_item_person = models.ForeignKey(Person, on_delete=models.PROTECT, related_name="items_found")
+    owner_item = models.ForeignKey(Person, on_delete=models.PROTECT, related_name="items_owned", null=True, blank=True)
